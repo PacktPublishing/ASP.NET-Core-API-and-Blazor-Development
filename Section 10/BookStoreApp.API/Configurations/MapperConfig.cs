fffe4d62dd2e928e94558af40e8d5683c9f69e92
sanjeetkumar13 <41476387+sanjeetkumar13@users.noreply.github.com>
2022-05-19 02:50:26 +0530

    Resource files added

---
+++ Section 10/BookStoreApp.API/Configurations/MapperConfig.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using BookStoreApp.API.Data;
+using BookStoreApp.API.Models.Author;
+using BookStoreApp.API.Models.Book;
+using BookStoreApp.API.Models.User;
+
+namespace BookStoreApp.API.Configurations
+{
+    public class MapperConfig : Profile
+    {
+        public MapperConfig()
+        {
+            CreateMap<AuthorReadOnlyDto, Author>().ReverseMap();
+            CreateMap<AuthorDetailsDto, Author>().ReverseMap();
+            CreateMap<AuthorCreateDto, Author>().ReverseMap();
+            CreateMap<AuthorUpdateDto, Author>().ReverseMap();
+
+            CreateMap<BookCreateDto, Book>().ReverseMap();
+            CreateMap<BookUpdateDto, Book>().ReverseMap();
+            CreateMap<Book, BookReadOnlyDto>()
+                .ForMember(q => q.AuthorName, d => d.MapFrom(map => $"{map.Author.FirstName} {map.Author.LastName}"))
+                .ReverseMap();
+
+            CreateMap<Book, BookDetailsDto>()
+                .ForMember(q => q.AuthorName, d => d.MapFrom(map => $"{map.Author.FirstName} {map.Author.LastName}"))
+                .ReverseMap();
+
+            CreateMap<ApiUser, UserDto>().ReverseMap();
+        }
+    }
+}
